ef4d8e306a5d992e25d8456dd79686c04d6c8d53
Ben Fung <benfung1996@gmail.com>
2020-04-20 13:52:51 -0700

    added html

---
+++ FriendFinder/app/data/friend.js
@@ -0,0 +1,9 @@
+var friendArray = [
+    {
+        name: "test",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1,2,3,4,5,4,3,2,1,5]
+    }
+];
+
+module.exports = friendArray;
