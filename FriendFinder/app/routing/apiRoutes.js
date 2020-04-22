e50b3dd809050b05e5d98671c20079af64b8dd8f
Ben Fung <benfung1996@gmail.com>
2020-04-21 17:38:36 -0700

    added api function logic

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -7,9 +7,36 @@ module.exports = function(app) {
     });
     
     app.post("/api/friends", function(req, res) {
+
+        var user = req.body;
+        var bestMatch = 0;
+        var minDiff = 40;
+
+        user.scores.forEach((element) => {
+            parseInt(element)
+        })
+
+        console.log(user)
+     
         for (var i = 0; i < friendsData.legnth; i++) {
-            
+            var totalDifference = 0;
+            for (var a = 0; a < this.friendsData.scores.length; a++) {
+                var diff = Math.abs(user.scores[a] - this.friendsData.scores[a]);
+                totalDifference += diff;
+                // console.log(totalDifference);
+            }
+
+            if (totalDifference < minDiff) {
+                bestMatch = i;
+                minDiff = totalDifference;
+                // console.log(minDiff);
+            }
+
         }
+
+        friendsData.push(user);
+        res.json(friendsData[bestMatch]);
+        // console.log(friendsData[bestMatch]);
     })
     
 }
